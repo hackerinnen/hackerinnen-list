958c9c3f376eaa81bb1e4ea70605a41c8e7d90c0
Kristin Fritsch <ich@kristin-fritsch.de>
2021-02-11 12:38:45 +0100

    Fix form submission

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,13 +1,15 @@
 <template>
   <button class="delete is-pulled-right" @click="$emit('close')"></button>
   <form
-    name="contact"
+    v-if="!submitted"
+    name="contact-form"
     method="POST"
     class="mt-5"
     netlify
     data-netlify-honeypot="bot-field"
+    @submit.prevent="onSubmit"
   >
-    <input type="hidden" name="form-name" value="contact" />
+    <input type="hidden" name="form-name" value="contact-form" />
     <p class="pr-4 pl-4">
       Du kennste eine Initiative, eine Webseite, einen Blog der hier fehlt? Dann
       lass es und wissen!
@@ -15,7 +17,11 @@
     <div class="field mt-3">
       <label class="label">Deine Nachricht</label>
       <div class="control">
-        <textarea class="textarea" name="message"></textarea>
+        <textarea
+          v-model="form.message"
+          class="textarea"
+          name="message"
+        ></textarea>
       </div>
     </div>
     <div class="field mt-3">
@@ -24,16 +30,62 @@
         <span class="icon is-small is-left">
           <i class="fas fa-envelope"></i>
         </span>
-        <input class="input" type="email" name="email" />
+        <input v-model="form.email" class="input" type="email" name="email" />
       </div>
       <p class="help">
         Falls du für Rückfragen zur Verfügung stehst. (Optional)
       </p>
     </div>
     <div class="control mt-6">
-      <button type="submit" class="button is-primary" @click="$emit('close')">
+      <button type="submit" class="button is-primary">
         Absenden
       </button>
     </div>
   </form>
+  <div v-else>
+    Thank you!
+    <button class="button is-primary" @click="$emit('close')">
+      Schließen
+    </button>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Form",
+  emits: ["close"],
+  data: function() {
+    return {
+      submitted: false,
+      form: {
+        message: "",
+        email: "",
+      },
+    };
+  },
+  methods: {
+    encode: function(data) {
+      return Object.keys(data)
+        .map(
+          (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        )
+        .join("&");
+    },
+    onSubmit: async function() {
+      try {
+        await fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: this.encode({
+            "form-name": "contact-form",
+            ...this.form,
+          }),
+        });
+        this.submitted = true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
