3f0f56b9f01f050d20bc73e94ae52bd07b5a2eb9
Kristin Fritsch <ich@kristin-fritsch.de>
2023-02-23 00:04:40 +0100

    Add filter

---
+++ astro/src/components/filterData.ts
@@ -0,0 +1,186 @@
+export const filterData = {
+  interests: [
+    {
+      key: "conference",
+      title: "Konferenzen",
+      selected: false,
+      tags: ["Konferenz"],
+    },
+    {
+      key: "initiative",
+      title: "Initiativen",
+      selected: false,
+      tags: ["Initiative"],
+    },
+    {
+      key: "hackspace",
+      title: "Hackspace",
+      selected: false,
+      tags: ["Hackspace"],
+    },
+    {
+      key: "network",
+      title: "Netzwerk",
+      selected: false,
+      tags: ["Netzwerk"],
+    },
+    {
+      key: "lokal",
+      title: "lokale Angebote",
+      selected: false,
+      tags: ["lokale Community", "lokale Angebote"],
+    },
+    {
+      key: "community",
+      title: "Communities",
+      selected: false,
+      tags: ["Community"],
+    },
+    {
+      key: "Vorbilder",
+      title: "Vorbilder",
+      selected: false,
+      tags: ["Vorbilder"],
+    },
+    {
+      key: "code",
+      title: "Programmieren lernen",
+      selected: false,
+      tags: ["Workshops", "Tutorial", "Kurse", "Bootcamp"],
+    },
+    {
+      key: "mentorship",
+      title: "Mentoring",
+      selected: false,
+      tags: ["Mentoring"],
+    },
+    {
+      key: "Sichtbarkeit",
+      title: "Sichtbarkeit",
+      selected: false,
+      tags: ["Sichtbarkeit"],
+    },
+    {
+      key: "mailingliste",
+      title: "Mailingliste",
+      selected: false,
+      tags: ["Mailingliste"],
+    },
+    {
+      key: "karriere",
+      title: "Karriere",
+      selected: false,
+      tags: ["Karriere"],
+    },
+  ],
+  format: [
+    // { key: "blog", title: "Blog", icon: "fas fa-blog", selected: false },
+    {
+      key: "newsletter",
+      title: "Newsletter",
+      icon: {
+        iconName: "inbox",
+        prefix: "fas",
+      },
+      selected: false,
+    },
+    {
+      key: "telegram",
+      title: "Telegram Gruppe",
+      icon: {
+        iconName: "telegram",
+        prefix: "fab",
+      },
+      selected: false,
+    },
+    {
+      key: "youtube",
+      title: "YouTube",
+      icon: {
+        iconName: "youtube",
+        prefix: "fab",
+      },
+      selected: false,
+    },
+    {
+      key: "twitch",
+      title: "Twitch",
+      icon: {
+        iconName: "twitch",
+        prefix: "fab",
+      },
+      selected: false,
+    },
+    // {
+    //   key: "tiktok",
+    //   title: "TikTok",
+    //   icon: "fab fa-tiktok",
+    //   selected: false,
+    // },
+    {
+      key: "podcast",
+      title: "Podcast",
+      icon: {
+        iconName: "podcast",
+        prefix: "fas",
+      },
+      selected: false,
+    },
+    {
+      key: "book",
+      title: "Buch",
+      icon: {
+        iconName: "book",
+        prefix: "fas",
+      },
+      selected: false,
+    },
+    {
+      key: "meetup",
+      title: "Meetup",
+      icon: {
+        iconName: "meetup",
+        prefix: "fab",
+      },
+      selected: false,
+    },
+  ],
+  social: [
+    {
+      key: "twitter",
+      title: "Twitter",
+      icon: {
+        iconName: "twitter",
+        prefix: "fab",
+      },
+      selected: false,
+    },
+    {
+      key: "instagram",
+      title: "Instagram",
+      icon: {
+        iconName: "instagram",
+        prefix: "fab",
+      },
+      selected: false,
+    },
+    {
+      key: "facebook",
+      title: "Facebook",
+      icon: {
+        iconName: "facebook",
+        prefix: "fab",
+      },
+      selected: false,
+    },
+  ],
+  language: [
+    { key: "de", title: "DE", selected: false },
+    { key: "en", title: "EN", selected: false },
+  ],
+  audience: [
+    { key: "pupil", title: "Schüler", selected: false },
+    // { key: "student", title: "Student", selected: false },
+    { key: "adult", title: "Ab 16", selected: false },
+  ],
+};
