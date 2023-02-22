823f529d7ef415cbbf96f4969f5a182cee925571
Kristin Fritsch <ich@kristin-fritsch.de>
2023-02-22 15:01:22 +0100

    Update cards

--- astro/src/types.ts
+++ astro/src/types.ts
@@ -1,17 +1,33 @@
-type AirtableFields = {
-  Cities: string[];
-  Webseite: string;
-  Beschreibung: string;
-  Newsletter: string;
-  Name: string;
-  Datum: string;
-  Tags: string[];
-  Sprache: string;
-  Region: string;
+type ResourceFields = {
+  name: string;
+  public: boolean;
+  brand: string;
+  description: string;
+  tags: string[];
+  website: string;
+  facebook: string;
+  twitter: string;
+  instagram: string;
+  newsletter: string;
+  youtube: string;
+  twitch: string;
+  tikTok: string;
+  podcast: string;
+  telegram: string;
+  meetup: string;
+  email: string;
+  blog: string;
+  book: string;
+  kids: boolean;
+  region: string;
+  cities: string[];
+  language: string;
+  "create date": string;
+  "update date": string;
 };
 
-export type AirtableRow = {
+export type Resource = {
   id: string;
   createdTime: string;
-  fields: AirtableFields;
+  fields: ResourceFields;
 };
